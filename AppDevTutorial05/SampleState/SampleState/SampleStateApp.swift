552b60770145a9b78483ce5cd675319ef9aa20de
sakiyamaK <sakiyama.k@gmail.com>
2025-05-15 10:23:58 +0900

    SampleStateを追加

---
+++ AppDevTutorial05/SampleState/SampleState/SampleStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleStateApp.swift
+//  SampleState
+//
+//  Created by sakiyamaK on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SampleStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SourceView()
+        }
+    }
+}
---
+++ AppDevTutorial05/SampleState/SampleState/SourceView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  SampleState
+//
+//  Created by sakiyamaK on 2025/05/15.
+//
+
+import SwiftUI
+
+struct SourceView: View {
+
+    @State var sourceValue: Int = 0
+    var body: some View {
+        VStack(spacing: 10) {
+            Button("+1するよ") {
+                sourceValue += 1
+            }
+            Text("Source Value \(sourceValue)")
+            CopyView(copyValue: sourceValue)
+            BindingView(bindingValue: $sourceValue)
+        }
+    }
+}
+
+struct CopyView: View {
+
+    @State var copyValue: Int = 0
+
+    var body: some View {
+        Text("Copy Value \(copyValue)")
+    }
+}
+
+struct BindingView: View {
+
+    @Binding var bindingValue: Int
+
+    var body: some View {
+        Text("Copy Value \(bindingValue)")
+    }
+}
+
+
+#Preview {
+    SourceView()
+}
